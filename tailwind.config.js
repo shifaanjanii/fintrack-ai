edb4be9c59b6ca6058aa26edde806cca41bff7fa
Shifa Anjani Desha <sanjani462@gmail.com>
2026-05-11 02:20:52 +0700

    Migrasi ke React Berhasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,23 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        // Menambahkan font Poppins biar UI-nya 100% mirip Figma
+        poppins: ['Poppins', 'sans-serif'],
+      },
       colors: {
+        // Warna asli dari Sipa
         'fintrack-bg': '#efeefc',
-        'fintrack-purple': '#8477e4',
         'fintrack-pink-light': '#feeefd',
         'fintrack-pink-dark': '#e584ee',
+
+        // Warna Kategori yang kita sepakati (Ungu, Hijau, Merah)
+        'fintrack-purple': '#8477e4', // Kebutuhan
+        'fintrack-green': '#10B981',  // Keinginan
+        'fintrack-red': '#EF4444',    // Tabungan
+        
+        // Warna kartu transparan (Glassmorphism)
+        'fintrack-card': 'rgba(255, 255, 255, 0.7)',
       }
     },
   },
